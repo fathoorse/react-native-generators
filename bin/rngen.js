56cd6a52eb1a42e637af4e5140f7e254feecf283
Fat Horse <fathoorse@gmail.com>
2018-02-02 17:46:22 +0100

    Add bin

---
+++ bin/rngen.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+
+const { rngen } = require('../index')
+
+rngen()
--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const { createComponent } = require('./source/creators')
 const { createReduxSkeleton, errorCodes } = require('./source/createReduxSkeleton')
 
-const args = process.argv.slice(2)
-const command = args[0]
+exports.rngen = () => {
+    const args = process.argv.slice(2)
+    const command = args[0]
 
-switch (command) {
-    case 'skeleton':
-        createReduxSkeleton(args[1]).catch(err => {
-            switch (err.code) {
-                case errorCodes.cantCreateRootFolder:
-                    console.log(`Folder '${err.info.path}' probably existed. Specify unique name`)
+    switch (command) {
+        case 'skeleton':
+            createReduxSkeleton(args[1]).catch(err => {
+                switch (err.code) {
+                    case errorCodes.cantCreateRootFolder:
+                        console.log(`Folder '${err.info.path}' probably existed. Specify unique name`)
+                }
+            })
+            break
+        case 'component':
+            if (args[1] === undefined) {
+                console.log("Need a name for component")
+                return
             }
-        })
-        break
-    case 'component':
-        if (args[1] === undefined) {
-            console.log("Need a name for component")
-            return
-        }
-        createComponent('', args[1])
-        break
-    case undefined:
-        console.log("Usage: react-native-generator command")
-    default:
-        console.log("We can't do it yet")
+            createComponent('', args[1])
+            break
+        case undefined:
+            console.log("Usage: react-native-generator command")
+        default:
+            console.log("We can't do it yet")
+    }
 }
